188fccce41894435dcf85114dbe8e18f7cedfa31
Anisha-Neupane <anishaneupane077@gmail.com>
2021-11-30 21:55:14 +0000

    Project 3

--- R/Simulations.R
+++ R/Simulations.R
@@ -1,1 +1,19 @@
 # I will carry out mcmc analysis for data simulation.
+#' @param n Number of extant sampled tips.
+#' @param numbsim Number of trees to simulate.
+#' @param lambda Vector of speciation rates, the rate in entry i is the speciation rate prior (ancestral) to time times[i].
+#' @param mu 	Vector of extinction rates, the rate in entry i is the extinction rate prior (ancestral) to time times[i].
+#' @param psi Vector of fossil sampling rates, the rate in entry i is the fossil sampling rate prior (ancestral) to time times[i].
+#' @param times Vector of mass extinction and rate shift times. Time is 0 today and increasing going backwards in time. Specify the vector as times[i]<times[i+1]. times[1]=0 (today).
+#' 
+
+all <- read_tsv("Data/ants_timevary.log"){ 
+all
+
+lambda <- all$speciation_rate
+mu <- all$extinction_rate
+psi <- all$psi
+times <- all$origin_time
+
+trees = sim.fbd.rateshift.taxa(n, numbsim, lambda, mu, psi, times)
+}
