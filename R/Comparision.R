a11e41a232c1611a8e1a89135175f4802e3df440
Anisha-Neupane <anishaneupane077@gmail.com>
2021-12-01 01:43:56 +0000

    Project 3

--- R/Comparision.R
+++ R/Comparision.R
@@ -1,1 +1,8 @@
-# I will use some test statistics which numerically summarize different aspects of dataset. The output wll be mean, median, 1st, and 90th percentile.
+# Comparision with emperical mean.
+for (i in 1:20)
+  simulated_data[i] <- trees[[i]]$root.edge
+
+emperical_data <- all$origin_time
+emp_mean <- mean(emperical_data)
+
+ggplot(data.frame(x = simulated_data), aes(x)) + geom_density() + geom_vline (xintercept = emp_mean, linetype = "dotted", col = 4)
--- R/posterior trace.R
+++ R/posterior trace.R
@@ -1,1 +1,5 @@
-# I will use function sim.fbd.taxa to simulate tree for data visualization.
+# I will use function rangeplot.asymmetric to simulate tree for data visualization.
+#' Data Visualization
+i=1
+for (i in 1:20)
+rangeplot.asymmetric(trees[[i]])
