2ebd82a60b389670ec52c55784750394fb86e7e1
Anisha-Neupane <anishaneupane077@gmail.com>
2021-12-02 21:04:18 +0000

    .

--- R/Simulations.R
+++ R/Simulations.R
@@ -1,16 +1,14 @@
 # I will use sim.fbd.taxa to simulate data.
 #' @param n Number of extant sampled tips.
 #' @param numbsim Number of trees to simulate.
-#' @param lambda Vector of speciation rates, the rate in entry i is the speciation rate prior (ancestral) to time times[i].
-#' @param mu 	Vector of extinction rates, the rate in entry i is the extinction rate prior (ancestral) to time times[i].
-#' @param psi Vector of fossil sampling rates, the rate in entry i is the fossil sampling rate prior (ancestral) to time times[i].
-#' @param times Vector of mass extinction and rate shift times. Time is 0 today and increasing going backwards in time. Specify the vector as times[i]<times[i+1]. times[1]=0 (today).
+#' @param frac fraction.
 #' @return trees[i]
 #' @usage 
-#' n = 10, numbsim = 2
+#' sim (10, 2 , 0.01)
 #' 
 
-all <- read_tsv("Data/ants_timevary.log"){  
+tre <- function(n, numbsim, frac) { 
+all <- read_tsv("Data/ants_timevary.log")   
 all
 
 lambda <- all$speciation_rate
@@ -20,5 +18,7 @@ times <- all$origin_time
 
 i=1
 while(i <= 20)
-  trees[i] <- (sim.fbd.taxa(n = 10, numbsim = 2, lambda[i], mu[i], psi[i], frac = .01))
-  i <- i +1 }
+trees[i] <- (sim.fbd.taxa(n, numbsim, lambda[i], mu[i], psi[i], frac))
+i <- i +1 
+return(trees)
+}
