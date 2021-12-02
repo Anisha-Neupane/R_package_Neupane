ad8a7265491b2079cbe2b6b5c75bb9a6c1fd8d85
Anisha-Neupane <anishaneupane077@gmail.com>
2021-12-02 20:56:35 +0000

    .

--- R/Simulations.R
+++ R/Simulations.R
@@ -5,17 +5,20 @@
 #' @param mu 	Vector of extinction rates, the rate in entry i is the extinction rate prior (ancestral) to time times[i].
 #' @param psi Vector of fossil sampling rates, the rate in entry i is the fossil sampling rate prior (ancestral) to time times[i].
 #' @param times Vector of mass extinction and rate shift times. Time is 0 today and increasing going backwards in time. Specify the vector as times[i]<times[i+1]. times[1]=0 (today).
+#' @return trees[i]
+#' @usage 
+#' n = 10, numbsim = 2
 #' 
 
-all <- read_tsv("Data/ants_timevary.log") 
+all <- read_tsv("Data/ants_timevary.log"){  
 all
 
-#lambda <- all$speciation_rate
-#mu <- all$extinction_rate
-#psi <- all$psi
-#times <- all$origin_time
+lambda <- all$speciation_rate
+mu <- all$extinction_rate
+psi <- all$psi
+times <- all$origin_time
 
 i=1
-while(i <= 20){
+while(i <= 20)
   trees[i] <- (sim.fbd.taxa(n = 10, numbsim = 2, lambda[i], mu[i], psi[i], frac = .01))
   i <- i +1 }
