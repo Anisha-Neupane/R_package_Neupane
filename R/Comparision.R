976b14c185f234d792baa624ee2af275c4ca3733
Anisha-Neupane <anishaneupane077@gmail.com>
2021-12-02 21:17:27 +0000

    .

--- R/Comparision.R
+++ R/Comparision.R
@@ -6,7 +6,7 @@
 for (i in 1:20){ 
 simulated_data[i] <- trees[[i]]$root.edge
 
-emperical_data <- all$origin_time
+
 emp_mean <- mean(emperical_data)
 }
 ggplot(data.frame(x = simulated_data), aes(x)) + geom_density() + geom_vline (xintercept = emp_mean, linetype = "dotted", col = 4)
