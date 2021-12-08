10805bc66ee87e525a75b548267f3c9fd842da1f
Anisha-Neupane <anishaneupane077@gmail.com>
2021-12-08 17:37:39 +0000

    .

--- R/Effect_size.R
+++ R/Effect_size.R
@@ -7,6 +7,6 @@
 #' Effect_size(datasdf = datas)
 #' 
 Effect_size <- function(datasdf) { 
-d = abs((mean - meane)/ ((((sd(emperical))^2 + (sd(simulated))^2)/2)^(1/2)))
+d = abs((mean - means)/ ((((sd(emperical_data))^2 + (sd(simulated_data))^2)/2)^(1/2)))
 eff_size = (d/((d^2+4))^(1/2))
 return(eff_size)}
