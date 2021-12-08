a820baac7450e8f47fd1ee13aeca5220d2bcdb27
Anisha-Neupane <anishaneupane077@gmail.com>
2021-12-08 18:07:15 +0000

    .

--- R/Comparision.R
+++ R/Comparision.R
@@ -1,16 +1,15 @@
 # Comparision of data
 #' use of ggplot
 #' @param datasdf Default dataset to use for plot.
-#' @return q
+#' @return p
 #' @export
 #' @usage 
-#' Comparision(datasdf = datas)
+#' comparision(datasdf = datas)
 
 
 
 comparision<- function(datasdf){
-p <- ggplot2::ggplot(datasdf, aes(fill = simulated, x = simulated)) +  ggplot2::geom_density()
-q <- p +ggplot2::geom_vline(aes(xintercept = mean, linetype = "dotted", col = 2))
-return(q)
-print(q)
+p <- ggplot2::ggplot(datasdf, aes(fill = simulated_data, x = simulated_data)) +  ggplot2::geom_density() + ggplot2::geom_vline(aes(xintercept = mean, linetype = "dotted", col = 2))
+return(p)
+print(p)
 }
